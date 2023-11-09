c4877d7ec5420fc1b8d976ee39560688d1df62e6
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-09 11:43:07 -0300

    Add files via upload

---
+++ NoteApp/NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Lucas Neves dos santos pompeu on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
