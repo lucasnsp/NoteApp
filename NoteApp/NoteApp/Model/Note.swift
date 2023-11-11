3ca405f46f03efa8a7c4e90cad8d9c762022d01a
Lucas Neves <lucas.neves12@icloud.com>
2023-11-11 18:05:42 -0300

    UserDefaults configured and now everytime the user makes something in the app he already changes the value and save locally.

--- NoteApp/NoteApp/Model/Note.swift
+++ NoteApp/NoteApp/Model/Note.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Note: Identifiable, Equatable {
+struct Note: Identifiable, Equatable, Codable {
     var id: UUID = UUID()
     var title: String = ""
     var content: String = ""
--- NoteApp/NoteApp/Notes/ViewModel/NoteViewModel.swift
+++ NoteApp/NoteApp/Notes/ViewModel/NoteViewModel.swift
@@ -9,7 +9,22 @@ import Foundation
 
 class NoteViewModel: ObservableObject {
     
-   @Published var notes: [Note] = [Note(title: "Titulo", content: "Descrição do conteudo")]
+    @Published var notes: [Note] = [] {
+        didSet {
+            saveNotes()
+        }
+    }
     
+    init() {
+        guard let data = UserDefaults.standard.data(forKey: "notes") else { return }
+        if let getNotes = try? JSONDecoder().decode([Note].self, from: data) {
+            self.notes = getNotes
+        }
+    }
     
+    func saveNotes() {
+        if let encoded = try? JSONEncoder().encode(notes) {
+            UserDefaults.standard.setValue(encoded, forKey: "notes")
+        }
+    }
 }
